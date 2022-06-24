e88d827ddaec96376a7547c287c861655f1e40f7
Francisco Toala <107563856+ecfrancisco@users.noreply.github.com>
2022-06-24 14:26:25 -0500

    Update process.php

--- process.php
+++ process.php
@@ -146,7 +146,7 @@ $json = json_decode($responseData, true);
 <script type='text/javascript' src="../bootstrap.min.js"></script>
 
 <body class="well">
-<script src="https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=<?php echo $json['id'] ?>"></script>
+<script src="https://test.oppwa.com/v1/paymentWidgets.js"></script>
 
 
 <div class="container">
